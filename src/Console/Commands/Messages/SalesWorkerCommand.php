5e6079f13074cd334791b20c50271a46bc53badd
Fábio Martins <fabiohbmartins@gmail.com>
2026-06-09 15:56:04 -0300

    Support RabbitMQ SSL configuration via `use_ssl` option in `sp-vendas` config.

--- src/Console/Commands/Messages/SalesWorkerCommand.php
+++ src/Console/Commands/Messages/SalesWorkerCommand.php
@@ -138,16 +138,12 @@ class SalesWorkerCommand extends Command
             'heartbeat' => $heartbeat
         ];
 
+        $useSsl = config('sp-vendas.rabbitmq.use_ssl', true);
         if (app()->isLocal()) {
-            $this->connection = new AMQPStreamConnection(
-                host: $host,
-                port: $port,
-                user: $user,
-                password: $password,
-                vhost: $virtualhost,
-                heartbeat: $heartbeat
-            );
-        } else {
+            $useSsl = false;
+        }
+
+        if ($useSsl) {
             $this->connection = new AMQPSSLConnection(
                 host: $host,
                 port: $port,
@@ -157,6 +153,17 @@ class SalesWorkerCommand extends Command
                 ssl_options: $sslOptions,
                 options: $options
             );
+
+            return;
         }
+
+        $this->connection = new AMQPStreamConnection(
+            host: $host,
+            port: $port,
+            user: $user,
+            password: $password,
+            vhost: $virtualhost,
+            heartbeat: $heartbeat
+        );
     }
 }
